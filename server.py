4fe4ae85057295c2ce11003a1176c0c8af12558d
Aakash Tembhare <aakash@valuebound.com>
2026-04-26 00:43:06 +0530

    fixes10

--- server.py
+++ server.py
@@ -57,6 +57,9 @@ def init_db():
     print(f"Startup Database created at {DB_PATH}")
 
 
+
+
+
 # ── EMAIL SHOOTER ───────────────────────────────────────────
 def send_confirmation_email(order_id: int, customer_name: str, customer_email: str, product: dict, quantity: int, total_price: int):
     """Sends a premium HTML order confirmation email."""
@@ -76,6 +79,11 @@ def send_confirmation_email(order_id: int, customer_name: str, customer_email: s
     from datetime import datetime, timedelta
     arriving_date = (datetime.now() + timedelta(days=3)).strftime("%A, %B %dth")
 
+
+
+
+
+
     # Premium HTML Version (Refined based on Nakpro/Shiprocket visuals)
     html_content = f"""
         <!DOCTYPE html>
@@ -87,15 +95,7 @@ def send_confirmation_email(order_id: int, customer_name: str, customer_email: s
             <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800&display=swap"
                 rel="stylesheet">
             <style>
-                body {
-
-                        margin: 0;
-                        padding: 0;
-                        background-color: #F1F5F9;
-                        font-family: 'Outfit', -apple-system, BlinkMacSystemFont, sans-serif;
-                        -webkit-font-smoothing: antialiased;
-                    }
-                
+                body { margin: 0;  padding: 0; background-color: #F1F5F9; font-family: 'Outfit', -apple-system, BlinkMacSystemFont, sans-serif; -webkit-font-smoothing: antialiased;}
             </style>
         </head>
         
